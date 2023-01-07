eda5b5e60e1657856d693f2c15890bfe26764467
Praveen Dias <praveendias1180@gmail.com>
2023-01-07 18:19:43 +0530

    Strings

---
+++ strings.rb
@@ -0,0 +1,14 @@
+# <string>.reverse
+test = "Praveen".reverse
+puts test
+
+# <string>.length
+puts "Praveen length is #{"Praveen".length}"
+
+# 5times = "Praveen" * 5
+puts "5times" * 5
+
+# <string>.size
+puts "Praveen size is #{"Praveen".size}"
+
+puts 40.to_s.reverse
