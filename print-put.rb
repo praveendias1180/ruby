d95fd59e7e35f1ce82e329047f69935dc6b821ec
Praveen Dias <praveendias1180@gmail.com>
2023-01-07 17:58:03 +0530

    print p and puts

---
+++ print-put.rb
@@ -0,0 +1,15 @@
+3.times do
+    print 'Welcome'
+end
+
+puts "@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+
+3.times do
+    p 'Welcome'
+end
+
+puts "@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+
+3.times do
+    puts 'Welcome'
+end
